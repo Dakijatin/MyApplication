48cb1ba5f96e01d5319407c57df87c58a7094688
Dakijatin <jatindaki9606@gmail.com>
2025-07-03 23:44:51 +0530

    [jd] bottom tab navigation

--- App.jsx
+++ App.jsx
@@ -4,8 +4,16 @@ import Profile from './src/screen/profile';
 import Search from './src/screen/search';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/AntDesign';
+import Feather from 'react-native-vector-icons/Feather';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
+
 
 const StackNavigator = () => {
   return(
@@ -17,11 +25,66 @@ const StackNavigator = () => {
   )
 }
 
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator initialRouteName='Home'
+      screenOptions={{
+        tabBarActiveTintColor: "red",
+        tabBarInactiveTintColor: "green",
+        tabBarLabelStyle: {
+          fontSize: 15
+        },
+        tabBarStyle: {
+          height: 80
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: () => (
+            <Icon name="home" color="#000" size={24} />
+          )
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: () => (
+            <Feather name="users" color="#000" size={24} />
+          )
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: () => (
+            <Fontisto name="search" color="#000" size={24} />
+          )
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Home" component={Home} />
+      <Drawer.Screen name="Profile" component={Profile} />
+      <Drawer.Screen name="Search" component={Search} />
+    </Drawer.Navigator>
+  );
+}
+
 const App = () => {
   return (
-   <NavigationContainer>
-      <StackNavigator/>
-   </NavigationContainer>
+    <NavigationContainer>
+      <TabNavigator />
+    </NavigationContainer>
   )
 }
 
--- src/screen/profile.jsx
+++ src/screen/profile.jsx
@@ -1,12 +1,11 @@
 import { Button, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const Profile = ({ navigation, route }) => {
-  const { id, name } = route.params;
+const Profile = ({ navigation}) => {
   return (
     <View>
       <Text style={{ margin: 20, fontSize: 20, fontWeight: 500 }}>Profile</Text>
-      <Text style={{ margin: 20, fontSize: 20, fontWeight: 500 }}>id : {id} ,Name: {name}</Text>
+      <Text style={{ margin: 20, fontSize: 20, fontWeight: 500 }}></Text>
       <Button title="Search" onPress={() => navigation.navigate("Search")} />
       <Text style={{ margin: 20, fontSize: 20, fontWeight: 500, textAlign: "center" }}>Welcome to Profile</Text>
 
